09f65735eda6993914ae7d85dd4d413b74bc679e
HanHyunJin-mis <37561451+HanHyunJin-mis@users.noreply.github.com>
2018-05-22 23:46:39 +0900

    Add files via upload

---
+++ homework1/js/square2.js
@@ -0,0 +1,34 @@
+//random squares
+var semo = document.getElementById("demo");
+var num = Math.floor(Math.random() * 20) + 30;
+
+
+//number만큼 반복
+//var i;
+//for (i = 0; i < num; i++) {
+    //document.write(i);
+//}
+
+while (i < num){i++}
+
+document.write(i);
+
+var myColor = "#AAAAAA";
+    document.write('
+        <style>
+            body{
+                background-color: '+myColor+';
+            }
+        </style>
+    ');
+
+var colorcode = '#' + Math.round(Math.random()*0xFFFFFF).toString(16);
+
+//Add squares
+
+
+
+//마우스 이벤트 click - gindex, dbclick - delete
+
+
+
